fa23536abf459af93b8c308e9eb5aac4181b2d66
Alex yang <2722223@qq.com>
2016-06-10 00:14:17 +0800

    Create Library

---
+++ PageObject/src/main/java/org/PageObject/App.java
@@ -0,0 +1,13 @@
+package org.PageObject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
