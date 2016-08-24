adffc32f716cbddc3083e02952d9f31aa4b5a1b3
chendong <chendonga@jiedaibao.com>
2016-08-24 13:44:27 +0800

    add 6 cases of home page's banner

--- PageObject/src/main/java/page/pc/homePage/PCHomePage.java
+++ PageObject/src/main/java/page/pc/homePage/PCHomePage.java
@@ -6,14 +6,35 @@ import org.openqa.selenium.support.FindBy;
 
 import basicTool.WaitTool;
 import page.pc.AbstractPCPage;
+import page.pc.BusinessVersionPage;
 
 public class PCHomePage extends AbstractPCPage {
 
     @FindBy(css = "div.downloads")
     private WebElement DownLoadButton;
     
-    @FindBy(css = "#downloadContainer>.buttonContainer>a[href='https://qiye.jiedaibao.com']")
-    private WebElement goQiYeButton;
+    
+    @FindBy(css = "#downloadContainer a[href*='qiye'] .innerContainer")
+    private WebElement goQiYeLink;
+    
+    @FindBy(css = ".pointContainr>:first-child")
+    private WebElement pointerFirstBannerButton;
+    
+    @FindBy(css = ".pointContainr>:last-child")
+    private WebElement pointerSecondBannerButton;
+    
+    @FindBy(css = "#downloadContainer .buttonContainer .downloadNow")
+    private WebElement downloadNowButton;
+    
+    @FindBy(css = "#toSeeVideo .custom-button-text")
+    private WebElement toViewVideoButton;
+    
+    @FindBy(css = "#my-video_html5_api")
+    private WebElement videoPlayArea;
+    
+    @FindBy(css = "#my-video .vjs-big-play-button")
+    private WebElement videoPlayButton;
+    
     
     @FindBy(css = "#part2>.container")
     private WebElement tempPart2Area;
@@ -30,15 +51,47 @@ public class PCHomePage extends AbstractPCPage {
     
     @FindBy(css = ".downloads>.btn-margin>.buttons-tip .android-download-btn")
     private WebElement sysDownloadButton_androidButton;
-
+    
+    
+    
     public PCHomePage(WebDriver driver) {
         super(driver);
         WaitTool.waitFor(driver, WaitTool.DEFAULT_WAIT_4_ELEMENT, DownLoadButton);
     }
 
-    public PCHomePage moveToDownloadNowBySystem() throws Exception {
-        return moveToElement(sysDownloadButton);
+    
+    
+    public PCHomePage pointerFirstBannerButton() throws Exception{
+         click(pointerFirstBannerButton, goQiYeLink);
+         return this;
     }
+    
+    public BusinessVersionPage goToBusinessVersionPage() throws Exception{
+        pointerFirstBannerButton();
+        return click(goQiYeLink, BusinessVersionPage.class);
+    }
+    
+    public PCHomePage pointerSecondBannerButton() throws Exception{
+        click(pointerSecondBannerButton, toViewVideoButton);
+        return this;
+    }
+    
+    public PCHomePage downloadNow() throws Exception{
+        pointerSecondBannerButton();
+        return moveToElement(downloadNowButton);
+    }
+    
+    public PCHomePage toWatchVideo() throws Exception{
+        pointerSecondBannerButton();
+        click(toViewVideoButton, videoPlayArea);
+        return this;
+    }
+    
+    public PCHomePage clickAndPlayVideo() throws Exception{
+        toWatchVideo();
+        return click(videoPlayButton, PCHomePage.class);
+    }
+    
     
     public PCHomePage moveToTempPart2() throws Exception{
         return moveToElement(tempPart2Area);
@@ -46,6 +99,10 @@ public class PCHomePage extends AbstractPCPage {
 
     public PCHomePage moveToDownloadNowByQRCode() throws Exception {
         return moveToElement(qrDownloadButton);
+    }
+    
+    public PCHomePage moveToDownloadNowBySystem() throws Exception {
+        return moveToElement(sysDownloadButton);
     }
     
     public PCHomePage moveToDownloadNowByiOS() throws Exception{
