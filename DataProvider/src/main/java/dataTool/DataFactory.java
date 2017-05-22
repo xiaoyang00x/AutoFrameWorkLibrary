6e4f4b5571f8ff0f742478815157698cd779b9de
Alex Yang <yangy@shicaidai.com>
2017-05-22 15:09:59 +0800

    add a demo for dubbo

--- DataProvider/src/main/java/dataTool/DataFactory.java
+++ DataProvider/src/main/java/dataTool/DataFactory.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DataFactory {
@@ -58,6 +59,91 @@ public class DataFactory {
         }
         throw new Exception("No data by use id in this yaml file!!!!");
 
+    }
+
+
+    /**
+     *
+     * @param model yaml data
+     * @param dto dubbo interface need paramater
+     * @param <T>
+     * @return a dto obj
+     * @throws Exception
+     */
+    public static <T> T tansferData(Object model, Class<T> dto) throws Exception {
+        Field[] field = model.getClass().getDeclaredFields();        //获取实体类的所有属性，返回Field数组
+        Object dtoBean = dto.newInstance();
+        for (int j = 0; j < field.length; j++) {     //遍历所有属性
+            String name = field[j].getName();    //获取属性的名字
+            if (name.equals("id")) {
+
+            } else {
+                name = name.substring(0, 1).toUpperCase() + name.substring(1); //将属性的首字符大写，方便构造get，set方法
+                String type = field[j].getGenericType().toString();    //获取属性的类型
+                if (type.equals("class java.lang.String")) {   //如果type是类类型，则前面包含"class "，后面跟类名
+                    Method m = model.getClass().getMethod("get" + name);
+                    String value = (String) m.invoke(model);    //调用getter方法获取属性值
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, String.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                if (type.equals("class java.lang.Integer")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    Integer value = (Integer) m.invoke(model);
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, Integer.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                if (type.equals("class java.lang.Short")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    Short value = (Short) m.invoke(model);
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, Short.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                if (type.equals("class java.lang.Double")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    Double value = (Double) m.invoke(model);
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, Double.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                if (type.equals("class java.lang.Boolean")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    Boolean value = (Boolean) m.invoke(model);
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, Boolean.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                if (type.equals("class java.util.Date")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    Date value = (Date) m.invoke(model);
+                    if (value != null) {
+                        Method set = dto.getMethod("set" + name, Date.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                
+                if (type.contains("java.util.List")) {
+                    Method m = model.getClass().getMethod("get" + name);
+                    List value = (List) m.invoke(model);
+                    if (value != null) {
+                        System.out.println("进来了");
+                        Method set = dto.getMethod("set" + name, List.class);
+                        set.invoke(dtoBean, value);
+                    }
+                }
+                
+                
+            }
+
+        }
+        return (T) dtoBean;
     }
 
 
--- DataProvider/src/main/java/dataTool/InitYamlFileUtil.java
+++ DataProvider/src/main/java/dataTool/InitYamlFileUtil.java
@@ -3,11 +3,14 @@ package dataTool;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.ho.yaml.Yaml;
 
-import dataBean.dubbo.productSys.mytestBean;
+import com.miaoqian.prod.api.dto.request.OrderQueryDto;
+
+import dataBean.dubbo.productSys.MyOrderQueryDto;
 
 public class InitYamlFileUtil {
 
@@ -28,15 +31,24 @@ public class InitYamlFileUtil {
     
     
     public static void main(String[] args) throws Exception {
-        mytestBean u = new mytestBean();
-        u.setId("mytest");
-        u.setName("13295412345");
-        u.setAge(19);
-        u.setBrithday(new Date());
-       // mytestBean u2 = DataFactory.getObject(mytestBean.class, "mytest");
-       // System.out.println(u2.getId());
-        initYamlFile(u);
-        System.out.println(u.getId());
+        List a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        MyOrderQueryDto m1 = new MyOrderQueryDto();
+        m1.setId("123");
+        m1.setProductTypeList(a);
+        m1.setTransferStatus(1);
+        m1.setStatus(1);
+        m1.setProductCode("1");
+        m1.setOrderSeqNo("12345677");
+        m1.setIsSettled(0);
+        m1.setBuyerUid("432143214321");
+        initYamlFile(m1);
+        
+        OrderQueryDto m2 = DataFactory.tansferData(m1, OrderQueryDto.class);
+        System.out.println(m2.toString());
+        
     }
 
 }
